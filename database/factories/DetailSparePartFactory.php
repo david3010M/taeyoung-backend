a4d140ce077cfd5b6405cfb028d349602551150c
david3010M <hvaldiviezos@unprg.edu.pe>
2024-08-01 13:45:45 -0500

    id country

--- database/factories/DetailSparePartFactory.php
+++ database/factories/DetailSparePartFactory.php
@@ -4,20 +4,18 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\DetailSparePart>
- */
 class DetailSparePartFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'quantity' => $this->faker->randomNumber(2),
+            'movementType' => $this->faker->randomElement(['purchase', 'sale']),
+            'purchasePrice' => $this->faker->randomFloat(2, 0, 1000),
+            'salePrice' => $this->faker->randomFloat(2, 0, 1000),
+            'order_id' => $this->faker->randomNumber(2),
+            'quotation_id' => $this->faker->randomNumber(2),
+            'spare_part_id' => $this->faker->randomNumber(1),
         ];
     }
 }
