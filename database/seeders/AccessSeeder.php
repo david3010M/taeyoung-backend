af693637c479b20fdc2f67ecf97441dd3bfa23e4
david3010M <hvaldiviezos@unprg.edu.pe>
2024-08-20 22:35:41 -0500

    accesos

--- database/seeders/AccessSeeder.php
+++ database/seeders/AccessSeeder.php
@@ -10,7 +10,7 @@ class AccessSeeder extends Seeder
 {
     public function run()
     {
-        TypeUser::find(1)->setAccess(1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]);
+        TypeUser::find(1)->setAccess(1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]);
         TypeUser::find(2)->setAccess(2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
     }
 }
