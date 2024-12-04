a4026a36248ea659294f2525e4b7c75e855a9f68
david3010M <hvaldiviezos@unprg.edu.pe>
2024-12-04 12:47:55 -0500

    Update GroupMenu.php

--- app/Models/GroupMenu.php
+++ app/Models/GroupMenu.php
@@ -48,6 +48,7 @@ class GroupMenu extends Model
                 $query->where('typeuser_id', $userTypeId);
             });
         }])
+        ->where('id', '!=', 4)
             ->get()
             ->map(function ($groupMenu) use ($userTypeId) {
                 // Filtrar optionMenus según el acceso del usuario
